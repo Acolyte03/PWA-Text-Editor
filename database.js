d0dcf41bc38daf8b92bf9743a3c72a7e7d665262
Acolyte03 <Acolyte03@gmail.com>
2023-12-07 12:46:01 -0600

    Fixed routes

--- database.js
+++ database.js
@@ -1,32 +1,32 @@
 
 import { openDB } from "idb";
-const initdb = async () => openDB("jate", 1, 
+const initdb = async () => openDB("PWA", 1, 
 {
     upgrade(db) 
     {
-        if (db.objectStoreNames.contains("jate")) 
+        if (db.objectStoreNames.contains("PWA")) 
         {
-            console.log("jate database already exists");
+            console.log("PWA database already exists");
             return;
         }
-        db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
-        console.log("jate database created");
+        db.createObjectStore("PWA", { keyPath: "id", autoIncrement: true });
+        console.log("PWA database created");
     },
 });
 export const putDb = async (content) => 
 {
-    const jateDB = await openDB("jate", 1);
-    const tx = jateDB.transaction("jate", "readwrite");
-    const store = tx.objectStore("jate");
+    const PWADB = await openDB("PWA", 1);
+    const tx = PWADB.transaction("PWA", "readwrite");
+    const store = tx.objectStore("PWA");
     const request = store.put({ id: 1, value: content });
     const result = await request;
     console.log("data saved to the database", result);
 };
 export const getDb = async () => 
 {
-    const jateDB = await openDB("jate", 1);
-    const tx = jateDB.transaction("jate", "readonly");
-    const store = tx.objectStore("jate");
+    const PWADB = await openDB("PWA", 1);
+    const tx = PWADB.transaction("PWA", "readonly");
+    const store = tx.objectStore("PWA");
     const request = store.getAll();
     const result = await request;
     console.log("data read from database", result);
